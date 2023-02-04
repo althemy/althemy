455032efc613e6a15d6cd5c68ac7e56d2bc96d70
engrhaider <meetengrhaider@gmail.com>
2023-02-04 10:07:05 +0500

    models_data.js file added.

---
+++ js/models_data.js
@@ -0,0 +1,22 @@
+let models_data = {
+    "dante": {
+        "title": "Dante Heks",
+        "description": "https://danteheks.althemy.org",
+        "cover_image": "https://althemy.github.io/althemy/images/covers/danteheks_altbg.jpg"
+    },
+    "obsidian_kerttu": {
+        "title": "Obsidian Kerttu",
+        "description": "https://danteheks.althemy.org",
+        "cover_image": ""
+    },
+    "psychara": {
+        "title": "Psychara",
+        "description": "https://psychara.althemy.org",
+        "cover_image": ""
+    },
+    "eleine": {
+        "title": "Eleine",
+        "description": "https://eleine.althemy.org",
+        "cover_image": ""
+    }
+}
