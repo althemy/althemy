4ea704e3244515fccbe69b8a97b1f0ac1c14e291
althemy <53301936+althemy@users.noreply.github.com>
2023-02-23 22:52:46 +0100

    Update models_data.js

--- js/models_data.js
+++ js/models_data.js
@@ -1,4 +1,5 @@
 let models_data = {
+    // Users with = URL + Cover + Avatar
     "dante_heks": {
         "title": "Dante Heks",
         "description": "https://danteheks.althemy.com",
@@ -23,4 +24,50 @@ let models_data = {
         "title": "Eleine",
         "description": "https://eleine.althemy.com",
     }
+    "ferasha": {
+        "title": "Ferasha",
+        "description": "https://ferashacosplay.althemy.com",
+    }
+    "wiccana": {
+        "title": "Wiccana",
+        "description": "https://wiccana.althemy.com",
+    }
+    "johanna": {
+        "title": "Johanna Vetis",
+        "description": "https://johannavetis.althemy.com",
+    }
+    "victoria poison": {
+        "title": "Victoria Poison",
+        "description": "https://victoriapoison.althemy.com",
+    }
+    "xilmordas": {
+        "title": "Xilmordas",
+        "description": "https://xilmordas.althemy.com",
+    }
+    "xilmordas": {
+        "title": "Xilmordas",
+        "description": "https://xilmordas.althemy.com",
+    }
+    "okkido": {
+        "title": "Okkido",
+        "description": "https://okkidocosplay.althemy.com",
+    }
+    "necrinity": {
+        "title": "Necrinity",
+        "description": "https://necrinity.althemy.com",
+    }
+    "irene astral": {
+        "title": "Irene Astral",
+        "description": "https://ireneastral.althemy.com",
+    }
+    "kali noir": {
+        "title": "Kali Noir",
+        "description": "https://kalinoir.althemy.com",
+    }
+    "magda corvinus": {
+        "title": "Magda Corvinus",
+        "description": "https://magdacorvinus.althemy.com",
+    }
+    // Users with = Cover + Avatar
+    // Users with =  Avatar
 }
