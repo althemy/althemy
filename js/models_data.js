39923c92679af5196ca2aeb99774ef03921c9b13
althemy <53301936+althemy@users.noreply.github.com>
2023-02-24 21:46:26 +0100

    Update models_data.js

--- js/models_data.js
+++ js/models_data.js
@@ -1,27 +1,28 @@
 let models_data = {
+    // Users with = URL + Cover + Avatar
     "dante_heks": {
         "title": "Dante Heks",
-        "description": "https://danteheks.althemy.org",
+        "description": "https://danteheks.althemy.com",
     },
     "obsidian_kerttu": {
         "title": "Obsidian Kerttu",
-        "description": "https://danteheks.althemy.org",
+        "description": "https://danteheks.althemy.com",
     },
     "katrin_lanfire": {
         "title": "Katrin Lanfire",
-        "description": "https://katrinlanfire.althemy.org",
+        "description": "https://katrinlanfire.althemy.com",
     },
     "psychara": {
         "title": "Psychara",
-        "description": "https://psychara.althemy.org",
+        "description": "https://psychara.althemy.com",
     },
     "tenshi": {
         "title": "Tenshi",
-        "description": "https://tenshi.althemy.org",
+        "description": "https://tenshi.althemy.com",
     },
     "eleine": {
         "title": "Eleine",
-        "description": "https://eleine.althemy.org",
+        "description": "https://eleine.althemy.com",
     },
     "ferasha": {
         "title": "Ferasha",
@@ -30,6 +31,92 @@ let models_data = {
     "wiccana": {
         "title": "Wiccana",
         "description": "https://wiccana.althemy.com",
-    }
+    },
+    "johanna": {
+        "title": "Johanna Vetis",
+        "description": "https://johannavetis.althemy.com",
+    },
+    "victoria poison": {
+        "title": "Victoria Poison",
+        "description": "https://victoriapoison.althemy.com",
+    },
+    "xilmordas": {
+        "title": "Xilmordas",
+        "description": "https://xilmordas.althemy.com",
+    },
+    "xilmordas": {
+        "title": "Xilmordas",
+        "description": "https://xilmordas.althemy.com",
+    },
+    "okkido": {
+        "title": "Okkido",
+        "description": "https://okkidocosplay.althemy.com",
+    },
+    "necrinity": {
+        "title": "Necrinity",
+        "description": "https://necrinity.althemy.com",
+    },
+    "irene astral": {
+        "title": "Irene Astral",
+        "description": "https://ireneastral.althemy.com",
+    },
+    "kali noir": {
+        "title": "Kali Noir",
+        "description": "https://kalinoir.althemy.com",
+    },
+    "allaparket": {
+        "title": "Alla Parket",
+        "description": "allaparket.althemy.com",
+    },
+    "santa evita": {
+        "title": "Santa Evita",
+        "description": "santa-evita.althemy.com",
+    },
+    "drusilla": {
+        "title": "Drusilla",
+        "description": "drusilla.althemy.com",
+    },
+    "murderous maria": {
+        "title": "Murderous Maria",
+        "description": "murderousmaria.althemy.com",
+    },
+    "silky": {
+        "title": "Silky",
+        "description": "silky.althemy.com",
+    },
+    "dragonfly": {
+        "title": "Dragonfly",
+        "description": "dragonfly.althemy.com",
+    },
+    "lilith vanderstorme": {
+        "title": "Lilith Vanderstorme",
+        "description": "vanderstorme.althemy.com",
+    },
+    "revena": {
+        "title": "Revena",
+        "description": "revena.althemy.com",
+    },
+    "victoria g wolf": {
+        "title": "Victoria G Wolf",
+        "description": "victoriagwolf.althemy.com",
+    },
+    "crimson rose": {
+        "title": "Crimson Rose",
+        "description": "crimsonrose.althemy.com",
+    },
+    "jossie ayame": {
+        "title": "Jossie Ayame",
+        "description": "jossieayame.althemy.com",
+    },
+    "chloe noir": {
+        "title": "Chloe Noir",
+        "description": "chloenoir.althemy.com",
+    },
 
+    "magda corvinus": {
+        "title": "Magda Corvinus",
+        "description": "https://magdacorvinus.althemy.com",
+    }
+    // Users with = Cover + Avatar
+    // Users with =  Avatar
 }
