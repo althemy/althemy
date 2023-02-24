fca36e04ec901d957bdb1d98b45942096f341a5a
althemy <53301936+althemy@users.noreply.github.com>
2023-02-24 06:16:59 +0100

    Update models_data.js

--- js/models_data.js
+++ js/models_data.js
@@ -1,72 +1,26 @@
 let models_data = {
-    
     "dante_heks": {
         "title": "Dante Heks",
-        "description": "https://danteheks.althemy.com",
+        "description": "https://danteheks.althemy.org",
     },
     "obsidian_kerttu": {
         "title": "Obsidian Kerttu",
-        "description": "https://danteheks.althemy.com",
+        "description": "https://danteheks.althemy.org",
     },
     "katrin_lanfire": {
         "title": "Katrin Lanfire",
-        "description": "https://katrinlanfire.althemy.com",
+        "description": "https://katrinlanfire.althemy.org",
     },
     "psychara": {
         "title": "Psychara",
-        "description": "https://psychara.althemy.com",
+        "description": "https://psychara.althemy.org",
     },
     "tenshi": {
         "title": "Tenshi",
-        "description": "https://tenshi.althemy.com",
+        "description": "https://tenshi.althemy.org",
     },
     "eleine": {
         "title": "Eleine",
-        "description": "https://eleine.althemy.com",
+        "description": "https://eleine.althemy.org",
     }
-    "ferasha": {
-        "title": "Ferasha",
-        "description": "https://ferashacosplay.althemy.com",
-    }
-    "wiccana": {
-        "title": "Wiccana",
-        "description": "https://wiccana.althemy.com",
-    }
-    "johanna": {
-        "title": "Johanna Vetis",
-        "description": "https://johannavetis.althemy.com",
-    }
-    "victoria poison": {
-        "title": "Victoria Poison",
-        "description": "https://victoriapoison.althemy.com",
-    }
-    "xilmordas": {
-        "title": "Xilmordas",
-        "description": "https://xilmordas.althemy.com",
-    }
-    "xilmordas": {
-        "title": "Xilmordas",
-        "description": "https://xilmordas.althemy.com",
-    }
-    "okkido": {
-        "title": "Okkido",
-        "description": "https://okkidocosplay.althemy.com",
-    }
-    "necrinity": {
-        "title": "Necrinity",
-        "description": "https://necrinity.althemy.com",
-    }
-    "irene astral": {
-        "title": "Irene Astral",
-        "description": "https://ireneastral.althemy.com",
-    }
-    "kali noir": {
-        "title": "Kali Noir",
-        "description": "https://kalinoir.althemy.com",
-    }
-    "magda corvinus": {
-        "title": "Magda Corvinus",
-        "description": "https://magdacorvinus.althemy.com",
-    }
-
 }
